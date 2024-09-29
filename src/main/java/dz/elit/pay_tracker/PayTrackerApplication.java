3b54a91432d654f30b92d3154541003836f2a391
Aissani khadidja <aissani.khadija@elit.dz>
2024-09-29 10:07:03 +0100

    Initial commit

---
+++ src/main/java/dz/elit/pay_tracker/PayTrackerApplication.java
@@ -0,0 +1,13 @@
+package dz.elit.pay_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dz/elit/pay_tracker/PayTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package dz.elit.pay_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
